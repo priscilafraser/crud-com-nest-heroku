971f1da3f827deec1c9823752d1bd5998099f57b
priscilafraser <priscila.fraser@gmail.com>
2021-09-21 21:17:05 -0400

    crud e conexao com mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Mongoose } from 'mongoose';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://fraser:4qx3xoO6Dko3NFNs@cluster0.yvygw.mongodb.net/db_nestComHeroku?retryWrites=true&w=majority'), UsersModule],   //importar mongoose module
   controllers: [],
   providers: [],
 })
